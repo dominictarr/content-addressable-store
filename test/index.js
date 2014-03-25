9c3f288fefa4d61d39fa746d92b0a95c5bc9aeca
Dominic Tarr <dominic.tarr@gmail.com>
2014-03-26 11:07:50 +1300

    test to get all keys

--- test/index.js
+++ test/index.js
@@ -4,14 +4,23 @@ var osenv  = require('osenv')
 var path   = require('path')
 var fs     = require('fs')
 var shasum = require('shasum')
+var rimraf = require('rimraf')
 
-var db = CAS(path.join(osenv.tmpdir(), 'test-cas'), 'sha1')
+var dir = path.join(osenv.tmpdir(), 'test-cas')
+rimraf.sync(dir)
+
+var db = CAS(dir, 'sha1')
+
+var helloHash = 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d'
+var emptyHash = 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+var abcHash   = 'a9993e364706816aba3e25717850c26c9cd0d89d'
+var readmeHash
 
 tape('add a value', function (t) {
   t.plan(2)
   db.add('hello', {encoding: 'utf8'}, function (err, hash) {
     if(err) throw err
-    t.equal(hash, 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d')
+    t.equal(hash, helloHash)
     db.get(hash, function (err, data) {
       if(err) throw err
       t.equal(shasum(data), hash)
@@ -23,7 +32,7 @@ tape('add a value', function (t) {
 tape('add the empty string', function (t) {
   db.add('', {encoding: 'utf8'}, function (err, hash) {
     if(err) throw err
-    t.equal(hash, 'da39a3ee5e6b4b0d3255bfef95601890afd80709')
+    t.equal(hash, emptyHash)
     db.get(hash, function (err, data) {
       if(err) throw err
       t.equal(shasum(data), hash)
@@ -35,13 +44,13 @@ tape('add the empty string', function (t) {
 tape('add twice & detect cached', function (t) {
   db.add('abc', {encoding: 'utf8'}, function (err, hash, cached) {
     if(err) throw err
-    t.equal(hash, 'a9993e364706816aba3e25717850c26c9cd0d89d')
-    t.notOk(cached)
+    t.equal(hash, abcHash)
+    t.notOk(cached, 'abc was not cached')
 
     db.add('abc', {encoding: 'utf8'}, function (err, hash, cached) {
       if(err) throw err
-      t.equal(hash, 'a9993e364706816aba3e25717850c26c9cd0d89d')
-      t.ok(cached)
+      t.equal(hash, abcHash)
+      t.ok(cached , 'abc was already cached')
       t.end()
     })
   })
@@ -54,7 +63,7 @@ tape('add a stream', function (t) {
   .pipe(db.addStream())
   .on('close', function () {
     var stream = this
-    console.log(stream.hash)
+    console.log(readmeHash = stream.hash)
     t.ok(stream.hash)
     db.get(stream.hash, function (err, content) {
       console.log('readme:', content.toString().split('\n').shift() + '...')
@@ -64,3 +73,16 @@ tape('add a stream', function (t) {
   })
 })
 
+tape('get all keys', function (t) {
+  db.all(function (err, all) {
+    if(err) throw err
+    console.error(all)
+    t.deepEqual(all, [
+      helloHash,
+      abcHash,
+      readmeHash,
+      emptyHash
+    ].sort())
+    t.end()
+  })
+})
